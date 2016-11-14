f82359422ea5876d325f8408bebcb71f1f85961a
Krzysztof Formanowicz <krzysztof.formanowicz@gft.com>
2016-11-14 15:29:11 +0100

    renamed spec

---
+++ src/test/groovy/test/ExamPlannerHomepageSpec.groovy
@@ -0,0 +1,24 @@
+package test
+
+import geb.spock.GebReportingSpec
+import page.ExamPlannerHomePage
+import page.LoginPage
+
+class ExamPlannerHomepageSpec extends GebReportingSpec {
+
+    def "Correct header is displayed"() {
+        when:
+        to ExamPlannerHomePage
+
+        then:
+        header == "GFT Poland"
+    }
+
+    def "Clicking on login button redirects to login page"() {
+        when:
+        via ExamPlannerHomePage
+
+        then:
+        loginButton.click(LoginPage)
+    }
+}
