9d581f394d21027c80d769bf0ada20a26d933019
Krzysztof Formanowicz <krzysztof.formanowicz@gft.com>
2016-11-14 15:28:30 +0100

    added dashboard page object and navbar module

---
+++ src/test/groovy/page/DashboardPage.groovy
@@ -0,0 +1,24 @@
+package page
+
+import geb.Page
+import page.module.NavbarModule
+
+class DashboardPage extends Page {
+    static url = "Dashboard/Index"
+
+    static at = {
+        title == "- ExamPlanner"
+        registeredTable.displayed
+        calendar.displayed
+    }
+
+    static content = {
+        addSessionButton { $("a", text: "Dodaj sesję") }
+        registerButton { $("a", text: "Dodaj zgłoszenie") }
+        todayButton { $("button", text: "Dziś") }
+        registeredTable { $("table") }
+        calendar { $("#calendar") }
+        navbar {$(".Navigation-list").module(NavbarModule)}
+    }
+}
+
