1c8a767ce9a73f3122cc0a2a01685e42ca663d8f
Krzysztof Formanowicz <krzysztof.formanowicz@gft.com>
2016-11-17 15:11:44 +0100

    Added session details sidebar module

---
+++ src/test/groovy/page/module/SidebarModule.groovy
@@ -0,0 +1,14 @@
+package page.module
+
+import geb.Module
+
+
+class SidebarModule extends Module{
+    static content = {
+        sessionStatus {$(".js-session-status").text()}
+        sessionDetailsLink {$("#sidebarItem-SessionDetails")}
+        sessionExamsLink {$("#sidebarItem-SessionExams")}
+        sessionNotesLink {$("#sidebarItem-SessionNotes")}
+        sessionAttachmentsLink {$("#sidebarIte-SessionAttachments")}
+    }
+}
